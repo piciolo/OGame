4470f1f197e798fd8f4ec668d041ffe182c4daa7
piciolo <piciolo2@gmail.com>
2026-04-20 08:42:26 +0200

    fix(152): add accordion toggle + official a.technology/a.prerequisites markup
    - h1 click ora espande/chiude la sezione (slideToggle jQuery)
    - UL della categoria dell'oggetto corrente visibile inline; altre nascoste
    - Markup LI aggiornato con <a class=technology> + <a class=prerequisites>
      per usare il layout flex 50/50 della CSS ufficiale
    - Classi stato fulfilled/unfulfilled (OGame ufficiale) al posto di built/notBuilt

--- resources/views/ingame/techtree/technology.blade.php
+++ resources/views/ingame/techtree/technology.blade.php
@@ -10,46 +10,46 @@
     @include('ingame.techtree.partials.nav', ['currentAction' => 'technologies', 'objectId' => $object->id])
 
     <div class="content technologies">
-        @foreach ($categories as $category)
+        @foreach ($categories as $categoryKey => $category)
+            @php
+                $containsOpen = false;
+                foreach ($category['objects'] as $catObj) {
+                    if ($catObj->id === $object->id) { $containsOpen = true; break; }
+                }
+            @endphp
             @if (empty($category['objects']))
                 @continue
             @endif
             <h1>{{ __($category['label_key']) }}</h1>
-            <ul class="technologies">
+            <ul data-category="{{ $categoryKey }}"{{ $containsOpen ? ' style=display:block' : '' }}>
                 @foreach ($category['objects'] as $categoryObject)
                     @php($isOpen = $categoryObject->id === $object->id)
                     <li class="{{ $categoryObject->class_name }}{{ $isOpen ? ' open' : '' }}">
                         <a href="{{ route('techtree.ajax', ['tab' => 3, 'object_id' => $categoryObject->id]) }}"
-                           class="sprite sprite_small small overlay {{ $categoryObject->class_name }} tooltipHTML"
+                           class="technology sprite sprite_small small overlay {{ $categoryObject->class_name }} tooltipHTML"
                            data-overlay-same="true"
                            title="{{ $categoryObject->title }}|{{ $categoryObject->description }}">
-                        </a>
-                        <a href="{{ route('techtree.ajax', ['tab' => 3, 'object_id' => $categoryObject->id]) }}"
-                           class="name overlay"
-                           data-overlay-same="true">
                             {{ $categoryObject->title }}
                         </a>
                         @if ($isOpen)
-                            <div class="requirements">
-                                @if (empty($open_requirements))
-                                    <span class="hint">{{ __('t_ingame.techtree.no_requirements') }}</span>
-                                @else
-                                    @foreach ($open_requirements as $requirement)
-                                        <a href="{{ route('techtree.ajax', ['tab' => 3, 'object_id' => $requirement->gameObject->id]) }}"
-                                           class="overlay {{ $requirement->levelCurrent >= $requirement->levelRequired ? 'built' : 'notBuilt' }}"
-                                           data-overlay-same="true">
-                                            {{ $requirement->gameObject->title }}
-                                            ({{ __('t_ingame.techtree.level') }}
-                                            @if ($requirement->levelCurrent >= $requirement->levelRequired)
-                                                {{ $requirement->levelRequired }}
-                                            @else
-                                                {{ $requirement->levelCurrent }}/{{ $requirement->levelRequired }}
-                                            @endif
-                                            )
-                                        </a>
-                                    @endforeach
-                                @endif
-                            </div>
+                            @if (empty($open_requirements))
+                                <a class="prerequisites"><span class="hint">{{ __('t_ingame.techtree.no_requirements') }}</span></a>
+                            @else
+                                @foreach ($open_requirements as $requirement)
+                                    <a href="{{ route('techtree.ajax', ['tab' => 3, 'object_id' => $requirement->gameObject->id]) }}"
+                                       class="prerequisites overlay sprite_small small {{ $requirement->gameObject->class_name }} {{ $requirement->levelCurrent >= $requirement->levelRequired ? 'fulfilled' : 'unfulfilled' }}"
+                                       data-overlay-same="true">
+                                        {{ $requirement->gameObject->title }}
+                                        ({{ __('t_ingame.techtree.level') }}
+                                        @if ($requirement->levelCurrent >= $requirement->levelRequired)
+                                            {{ $requirement->levelRequired }}
+                                        @else
+                                            {{ $requirement->levelCurrent }}/{{ $requirement->levelRequired }}
+                                        @endif
+                                        )
+                                    </a>
+                                @endforeach
+                            @endif
                         @endif
                     </li>
                 @endforeach
@@ -59,9 +59,10 @@
 </div>
 
 <script type="text/javascript">
-    $(
-        function(){
-            initOverlayName();
-        }
-    );
+    $(function(){
+        initOverlayName();
+        $('#technologytree .content.technologies > h1').off('click.techtree').on('click.techtree', function(){
+            $(this).next('ul').slideToggle(150);
+        });
+    });
 </script>
