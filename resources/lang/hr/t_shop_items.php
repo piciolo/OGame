a62706d15c146f6fb8e7bf8224bed37e3d576795
piciolo <piciolo2@gmail.com>
2026-04-28 00:06:37 +0200

    feat(i18n): add HR (Croatian) Auctioneer + Shop translations from official OGame
    Source: official OGame s274-it.ogame.gameforge.com (UI=hr) - Aukcionar + Trgovina

---
+++ resources/lang/hr/t_shop_items.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+    // Categories
+    'category_special_offers' => 'Posebne ponude',
+    'category_class_selection' => 'Odabir Klase',
+    'category_construction' => 'Izgradnja',
+    'category_resources' => 'Resursi',
+    'category_booster30' => 'Pojačivač (30 dana)',
+    'category_booster90' => 'Pojačivač (90 dana)',
+    'category_profile' => 'Profil',
+    'category_items' => 'svi',
+
+    // Tabs
+    'tab_shop' => 'Trgovina',
+    'tab_inventory' => 'Inventar',
+    'page_title_shop' => 'Trgovina',
+    'page_title_inventory' => 'Inventar',
+
+    // Buttons
+    'btn_get_more_resources' => 'Nabavi više resursa',
+    'btn_purchase_dark_matter' => 'Naruči Crnu Materiju',
+    'btn_activate' => 'Aktiviraj',
+    'btn_buy_and_activate' => 'Kupi i aktiviraj',
+    'btn_buy_at_cost' => 'Kupi za',
+
+    // Tiers
+    'tier_bronze' => 'Brončani',
+    'tier_silver' => 'Srebrni',
+    'tier_gold' => 'Zlatni',
+    'tier_platinum' => 'Platinasti',
+
+    // Labels
+    'label_duration' => 'Trajanje',
+    'label_inventory' => 'U inventaru',
+    'label_inventory_short' => 'Inventar',
+    'label_time_left' => 'Preostalo vrijeme',
+    'label_phase_in_use' => 'Trenutna faza: U uporabi',
+    'btn_extend' => 'Produži',
+    'label_price' => 'Cijena',
+    'overlay_title' => 'Aktivacijski predmeti',
+    'active_boosts_title' => 'Aktivni pojačivači',
+    'tooltip_rules_title' => 'Pravila',
+    'shop_link' => 'U Trgovinu/Inventar',
+    'duration_instant' => 'sada',
+    'duration_week' => '1 tjedan',
+    'unit_week' => 'tjedan',
+    'unit_day' => 'dani',
+
+    // Item titles
+    'booster_kraken_title' => 'Kraken',
+    'booster_newtron_title' => 'Newtron',
+    'booster_detroid_title' => 'Detroid',
+    'amplifier_metal_title' => 'Pojačivač metala',
+    'amplifier_crystal_title' => 'Pojačivač kristala',
+    'amplifier_deuterium_title' => 'Pojačivač deuterija',
+    'amplifier_energy_title' => 'Pojačivač energije',
+    'resources_lot_title' => 'Paket resursa',
+    'planet_fields_title' => 'Polja planeta',
+    'moon_fields_title' => 'Polja mjeseca',
+    'expedition_slot_title' => 'Slotovi ekspedicije',
+    'fleet_slot_title' => 'Slotovi flote',
+
+    // Item descriptions
+    'booster_kraken_desc' => 'Smanjuje (:reduction) vrijeme izgradnje zgrada koje se trenutno grade na planetu.',
+    'booster_newtron_desc' => 'Smanjuje za :reduction vrijeme istraživanja trenutno aktivnih istraživanja.',
+    'booster_detroid_desc' => 'Smanjuje (:reduction) vrijeme izvršavanja zadataka Brodogradilišta koji se trenutno odvijaju na planetu.',
+    'amplifier_metal_desc' => '+:percent% više iskopa Rudnika metala na jednom planetu',
+    'amplifier_crystal_desc' => '+:percent% više proizvodnje Rudnika kristala na jednom planetu',
+    'amplifier_deuterium_desc' => '+:percent% više iskopa Sintetizatora deuterija na jednom planetu',
+    'amplifier_energy_desc' => '+:percent% više proizvodnje energije na planetu',
+    'resources_lot_desc' => 'Primi fiksni paket resursa: :metal metala, :crystal kristala, :deuterium deuterija.',
+    'planet_fields_desc' => 'Trajno povećava veličinu planeta za :fields polja.',
+    'moon_fields_desc' => 'Trajno povećava veličinu mjeseca za :fields polja.',
+    'expedition_slot_desc' => 'Povećava broj istovremenih ekspedicija za :slots.',
+    'fleet_slot_desc' => 'Povećava broj istovremenih flota za :slots.',
+
+    // Activation messages
+    'activate_success' => 'Predmet aktiviran.',
+    'activate_not_found' => 'Predmet nije dostupan u inventaru.',
+    'activate_not_supported' => 'Aktivacija još nije dostupna za ovaj predmet.',
+    'activate_no_target' => 'Nema valjane mete za aktivaciju (prazni red ili planet nedostupan).',
+
+    // Empty states
+    'inventory_empty' => 'Vaš inventar je prazan.',
+    'shop_coming_soon' => 'Trgovina Crne Materije bit će uskoro dostupna.',
+    'warning_storage_full' => 'Pažnja: skladište je puno. Paket se ne može aktivirati!',
+
+    // Buy messages
+    'buy_success' => 'Kupnja završena. Predmet pronađite u svom inventaru.',
+    'buy_insufficient_dm' => 'Nedovoljno Crne Materije.',
+    'label_dark_matter' => 'Crna Materija',
+    'btn_buy' => 'Kupi',
+    'btn_confirm_buy' => 'Potvrdi kupnju',
+    'price' => 'Cijena',
+];
