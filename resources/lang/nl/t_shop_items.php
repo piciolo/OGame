7703501a17cffc5bf3150d14713e4af04ed683bd
piciolo <piciolo2@gmail.com>
2026-04-27 23:40:10 +0200

    feat(i18n): add NL Auctioneer + Shop translations from official OGame
    Source: official OGame s274-it.ogame.gameforge.com (UI=nl) - Veilingmeester + Winkel
    
    Visual test on localhost (lang=nl) verified:
      - /shop sidebar renders 'Speciale aanbiedingen', 'Klasse Keuze', 'Constructie',
        'Grondstoffen', 'Booster (30/90 Dagen)' + tabs 'Winkel' / 'Inventaris'
      - /auctioneer renders 'Veilingmeester', warning 'Let op, geboden grondstoffen
        worden niet geretourneerd.'

---
+++ resources/lang/nl/t_shop_items.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+    // Categories
+    'category_special_offers' => 'Speciale aanbiedingen',
+    'category_class_selection' => 'Klasse Keuze',
+    'category_construction' => 'Constructie',
+    'category_resources' => 'Grondstoffen',
+    'category_booster30' => 'Booster (30 Dagen)',
+    'category_booster90' => 'Booster (90 Dagen)',
+    'category_profile' => 'Profiel',
+    'category_items' => 'Alle',
+
+    // Tabs
+    'tab_shop' => 'Winkel',
+    'tab_inventory' => 'Inventaris',
+    'page_title_shop' => 'Winkel',
+    'page_title_inventory' => 'Inventaris',
+
+    // Buttons
+    'btn_get_more_resources' => 'Krijg meer grondstoffen',
+    'btn_purchase_dark_matter' => 'Donkere Materie bestellen',
+    'btn_activate' => 'Activeren',
+    'btn_buy_and_activate' => 'Kopen & Activeren',
+    'btn_buy_at_cost' => 'Kopen voor',
+
+    // Tiers
+    'tier_bronze' => 'Brons',
+    'tier_silver' => 'Zilver',
+    'tier_gold' => 'Goud',
+    'tier_platinum' => 'Platina',
+
+    // Labels
+    'label_duration' => 'Duur',
+    'label_inventory' => 'In Inventaris',
+    'label_inventory_short' => 'Inventaris',
+    'label_time_left' => 'Resterende tijd',
+    'label_phase_in_use' => 'Huidige fase: In gebruik',
+    'btn_extend' => 'Verlengen',
+    'label_price' => 'Prijs',
+    'overlay_title' => 'Activeerbare items',
+    'active_boosts_title' => 'Actieve boosts',
+    'tooltip_rules_title' => 'Regels',
+    'shop_link' => 'Naar Winkel/Inventaris',
+    'duration_instant' => 'nu',
+    'duration_week' => '1 week',
+    'unit_week' => 'week',
+    'unit_day' => 'dagen',
+
+    // Item titles
+    'booster_kraken_title' => 'Kraken',
+    'booster_newtron_title' => 'Newtron',
+    'booster_detroid_title' => 'Detroid',
+    'amplifier_metal_title' => 'Metaal Booster',
+    'amplifier_crystal_title' => 'Kristal Booster',
+    'amplifier_deuterium_title' => 'Deuterium Booster',
+    'amplifier_energy_title' => 'Energie Booster',
+    'resources_lot_title' => 'Grondstoffenpakket',
+    'planet_fields_title' => 'Planeetvelden',
+    'moon_fields_title' => 'Maanvelden',
+    'expedition_slot_title' => 'Expeditievakken',
+    'fleet_slot_title' => 'Vlootvakken',
+
+    // Item descriptions
+    'booster_kraken_desc' => 'Verkort (:reduction) de bouwtijd van gebouwen die momenteel op een planeet worden gebouwd.',
+    'booster_newtron_desc' => 'Verkort met :reduction de onderzoekstijd van actieve onderzoeken.',
+    'booster_detroid_desc' => 'Verkort (:reduction) de uitvoertijd van scheepswerf-opdrachten die momenteel op een planeet bezig zijn.',
+    'amplifier_metal_desc' => '+:percent% meer Metaalmijnproductie op één planeet',
+    'amplifier_crystal_desc' => '+:percent% meer Kristalmijnproductie op één planeet',
+    'amplifier_deuterium_desc' => '+:percent% meer Deuteriumsynthesizer-productie op één planeet',
+    'amplifier_energy_desc' => '+:percent% meer energieproductie op een planeet',
+    'resources_lot_desc' => 'Ontvang een vast pakket grondstoffen: :metal metaal, :crystal kristal, :deuterium deuterium.',
+    'planet_fields_desc' => 'Vergroot de planeetgrootte permanent met :fields velden.',
+    'moon_fields_desc' => 'Vergroot de maangrootte permanent met :fields velden.',
+    'expedition_slot_desc' => 'Verhoogt het aantal gelijktijdige expedities met :slots.',
+    'fleet_slot_desc' => 'Verhoogt het aantal gelijktijdige vloten met :slots.',
+
+    // Activation messages
+    'activate_success' => 'Item geactiveerd.',
+    'activate_not_found' => 'Item niet beschikbaar in inventaris.',
+    'activate_not_supported' => 'Activatie nog niet beschikbaar voor dit item.',
+    'activate_no_target' => 'Geen geldig doel voor activatie (lege wachtrij of planeet niet beschikbaar).',
+
+    // Empty states
+    'inventory_empty' => 'Je inventaris is leeg.',
+    'shop_coming_soon' => 'De Donkere Materie-winkel is binnenkort beschikbaar.',
+    'warning_storage_full' => 'Let op: opslag is vol. Pakket kan niet worden geactiveerd!',
+
+    // Buy messages
+    'buy_success' => 'Aankoop voltooid. Het item bevindt zich in je inventaris.',
+    'buy_insufficient_dm' => 'Onvoldoende Donkere Materie.',
+    'label_dark_matter' => 'Donkere Materie',
+    'btn_buy' => 'Kopen',
+    'btn_confirm_buy' => 'Aankoop bevestigen',
+    'price' => 'Prijs',
+];
