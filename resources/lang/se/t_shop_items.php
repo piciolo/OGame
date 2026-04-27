d92fe1da3fdc12da8adbb84d7ae48af4fb48eb0c
piciolo <piciolo2@gmail.com>
2026-04-28 00:01:28 +0200

    feat(i18n): add SE Auctioneer + Shop translations from official OGame
    Source: official OGame s274-it.ogame.gameforge.com (UI=se) - Auktionsförrättare + Shop

---
+++ resources/lang/se/t_shop_items.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+    // Categories
+    'category_special_offers' => 'Speciella erbjudanden',
+    'category_class_selection' => 'Klass Val',
+    'category_construction' => 'Konstruktion',
+    'category_resources' => 'Resurser',
+    'category_booster30' => 'Booster (30 Dagar)',
+    'category_booster90' => 'Booster (90 Dagar)',
+    'category_profile' => 'Profile',
+    'category_items' => 'alla',
+
+    // Tabs
+    'tab_shop' => 'Shop',
+    'tab_inventory' => 'Lager',
+    'page_title_shop' => 'Shop',
+    'page_title_inventory' => 'Lager',
+
+    // Buttons
+    'btn_get_more_resources' => 'Skaffa mer resurser',
+    'btn_purchase_dark_matter' => 'Köp mörk materia',
+    'btn_activate' => 'Aktivera',
+    'btn_buy_and_activate' => 'Köp och Aktivera',
+    'btn_buy_at_cost' => 'Köp till priset',
+
+    // Tiers
+    'tier_bronze' => 'Brons',
+    'tier_silver' => 'Silver',
+    'tier_gold' => 'Guld',
+    'tier_platinum' => 'Platina',
+
+    // Labels
+    'label_duration' => 'Varaktighet',
+    'label_inventory' => 'I lagret',
+    'label_inventory_short' => 'Lager',
+    'label_time_left' => 'Återstående tid',
+    'label_phase_in_use' => 'Aktuell fas: Används',
+    'btn_extend' => 'Förläng',
+    'label_price' => 'Pris',
+    'overlay_title' => 'Aktiverbara objekt',
+    'active_boosts_title' => 'Aktiva boosters',
+    'tooltip_rules_title' => 'Regler',
+    'shop_link' => 'Till Shop/Lager',
+    'duration_instant' => 'nu',
+    'duration_week' => '1 vecka',
+    'unit_week' => 'vecka',
+    'unit_day' => 'dagar',
+
+    // Item titles
+    'booster_kraken_title' => 'Kraken',
+    'booster_newtron_title' => 'Newtron',
+    'booster_detroid_title' => 'Detroid',
+    'amplifier_metal_title' => 'Metall Booster',
+    'amplifier_crystal_title' => 'Kristall Booster',
+    'amplifier_deuterium_title' => 'Deuterium Booster',
+    'amplifier_energy_title' => 'Energi Booster',
+    'resources_lot_title' => 'Resurspaket',
+    'planet_fields_title' => 'Planetfält',
+    'moon_fields_title' => 'Månfält',
+    'expedition_slot_title' => 'Expeditionplatser',
+    'fleet_slot_title' => 'Flottplatser',
+
+    // Item descriptions
+    'booster_kraken_desc' => 'Förkortar (:reduction) byggtiden för byggnader som för närvarande byggs på en planet.',
+    'booster_newtron_desc' => 'Förkortar med :reduction forskningstiden för aktiva forskningar.',
+    'booster_detroid_desc' => 'Förkortar (:reduction) genomförandetiden för Skeppsvarvets uppdrag som för närvarande pågår på en planet.',
+    'amplifier_metal_desc' => '+:percent% mer Metallgruvproduktion på en planet',
+    'amplifier_crystal_desc' => '+:percent% mer Kristallgruvproduktion på en planet',
+    'amplifier_deuterium_desc' => '+:percent% mer Deuteriumsyntetiserarproduktion på en planet',
+    'amplifier_energy_desc' => '+:percent% mer energiproduktion på en planet',
+    'resources_lot_desc' => 'Få ett fast resurspaket: :metal metall, :crystal kristall, :deuterium deuterium.',
+    'planet_fields_desc' => 'Ökar planetstorleken permanent med :fields fält.',
+    'moon_fields_desc' => 'Ökar månstorleken permanent med :fields fält.',
+    'expedition_slot_desc' => 'Ökar antalet samtidiga expeditioner med :slots.',
+    'fleet_slot_desc' => 'Ökar antalet samtidiga flottor med :slots.',
+
+    // Activation messages
+    'activate_success' => 'Objekt aktiverat.',
+    'activate_not_found' => 'Objektet är inte tillgängligt i lagret.',
+    'activate_not_supported' => 'Aktivering är ännu inte tillgänglig för detta objekt.',
+    'activate_no_target' => 'Inget giltigt mål för aktivering (tom kö eller planet otillgänglig).',
+
+    // Empty states
+    'inventory_empty' => 'Ditt lager är tomt.',
+    'shop_coming_soon' => 'Mörk materia-shoppen kommer snart att vara tillgänglig.',
+    'warning_storage_full' => 'Varning: lagret är fullt. Paketet kan inte aktiveras!',
+
+    // Buy messages
+    'buy_success' => 'Köp slutfört. Hitta objektet i ditt lager.',
+    'buy_insufficient_dm' => 'Otillräckligt med mörk materia.',
+    'label_dark_matter' => 'Mörk materia',
+    'btn_buy' => 'Köp',
+    'btn_confirm_buy' => 'Bekräfta köp',
+    'price' => 'Pris',
+];
