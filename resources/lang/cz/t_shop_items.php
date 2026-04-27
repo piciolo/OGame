92225fe50821f83453805a78f5b8ffa482f3a4a3
piciolo <piciolo2@gmail.com>
2026-04-27 23:57:34 +0200

    feat(i18n): add CZ Auctioneer + Shop translations from official OGame
    Source: official OGame s274-it.ogame.gameforge.com (UI=cz) - Dražitel + Obchod
    
    Visual test on localhost (lang=cz) verified:
      - /shop sidebar renders 'Speciální nabídky', 'Výběr třídy', 'Konstrukce',
        'Suroviny', 'Posilovač (30/90 dní)' + tabs 'Obchod' / 'Inventář'
      - /auctioneer renders 'Dražitel'

---
+++ resources/lang/cz/t_shop_items.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+    // Categories
+    'category_special_offers' => 'Speciální nabídky',
+    'category_class_selection' => 'Výběr třídy',
+    'category_construction' => 'Konstrukce',
+    'category_resources' => 'Suroviny',
+    'category_booster30' => 'Posilovač (30 dní)',
+    'category_booster90' => 'Posilovač (90 dní)',
+    'category_profile' => 'Profil',
+    'category_items' => 'Vše',
+
+    // Tabs
+    'tab_shop' => 'Obchod',
+    'tab_inventory' => 'Inventář',
+    'page_title_shop' => 'Obchod',
+    'page_title_inventory' => 'Inventář',
+
+    // Buttons
+    'btn_get_more_resources' => 'Získat více surovin',
+    'btn_purchase_dark_matter' => 'Nákup Temné Hmoty',
+    'btn_activate' => 'Aktivovat',
+    'btn_buy_and_activate' => 'Koupit a aktivovat',
+    'btn_buy_at_cost' => 'Koupit za',
+
+    // Tiers
+    'tier_bronze' => 'Bronz',
+    'tier_silver' => 'Stříbro',
+    'tier_gold' => 'Zlato',
+    'tier_platinum' => 'Platina',
+
+    // Labels
+    'label_duration' => 'Doba trvání',
+    'label_inventory' => 'V Inventáři',
+    'label_inventory_short' => 'Inventář',
+    'label_time_left' => 'Zbývající čas',
+    'label_phase_in_use' => 'Aktuální fáze: V použití',
+    'btn_extend' => 'Prodloužit',
+    'label_price' => 'Cena',
+    'overlay_title' => 'Aktivovatelné předměty',
+    'active_boosts_title' => 'Aktivní posilovače',
+    'tooltip_rules_title' => 'Pravidla',
+    'shop_link' => 'Do Obchodu/Inventáře',
+    'duration_instant' => 'okamžitě',
+    'duration_week' => '1 týden',
+    'unit_week' => 'týden',
+    'unit_day' => 'dny',
+
+    // Item titles
+    'booster_kraken_title' => 'Kraken',
+    'booster_newtron_title' => 'Newtron',
+    'booster_detroid_title' => 'Detroid',
+    'amplifier_metal_title' => 'Posilovač kovu',
+    'amplifier_crystal_title' => 'Posilovač krystalu',
+    'amplifier_deuterium_title' => 'Posilovač deuteria',
+    'amplifier_energy_title' => 'Posilovač energie',
+    'resources_lot_title' => 'Balíček surovin',
+    'planet_fields_title' => 'Pole planety',
+    'moon_fields_title' => 'Pole měsíce',
+    'expedition_slot_title' => 'Sloty expedice',
+    'fleet_slot_title' => 'Sloty flotily',
+
+    // Item descriptions
+    'booster_kraken_desc' => 'Zkrátí (:reduction) dobu výstavby budov, které jsou aktuálně ve výstavbě na planetě.',
+    'booster_newtron_desc' => 'Zkrátí o :reduction dobu výzkumu právě probíhajících výzkumů.',
+    'booster_detroid_desc' => 'Zkrátí (:reduction) dobu provádění úloh Loděnice, které jsou aktuálně v běhu na planetě.',
+    'amplifier_metal_desc' => '+:percent% více těžby Dolu na kov na jedné planetě',
+    'amplifier_crystal_desc' => '+:percent% více produkce Dolu na krystaly na jedné planetě',
+    'amplifier_deuterium_desc' => '+:percent% více těžby Syntetizéru deuteria na jedné planetě',
+    'amplifier_energy_desc' => '+:percent% více produkce energie na planetě',
+    'resources_lot_desc' => 'Obdrž pevný balíček surovin: :metal kovu, :crystal krystalu, :deuterium deuteria.',
+    'planet_fields_desc' => 'Trvale zvětší velikost planety o :fields polí.',
+    'moon_fields_desc' => 'Trvale zvětší velikost měsíce o :fields polí.',
+    'expedition_slot_desc' => 'Zvýší počet současných expedic o :slots.',
+    'fleet_slot_desc' => 'Zvýší počet současných flotil o :slots.',
+
+    // Activation messages
+    'activate_success' => 'Předmět aktivován.',
+    'activate_not_found' => 'Předmět není v inventáři k dispozici.',
+    'activate_not_supported' => 'Aktivace pro tento předmět ještě není k dispozici.',
+    'activate_no_target' => 'Žádný platný cíl pro aktivaci (prázdná fronta nebo planeta nedostupná).',
+
+    // Empty states
+    'inventory_empty' => 'Tvůj inventář je prázdný.',
+    'shop_coming_soon' => 'Obchod s Temnou Hmotou bude brzy k dispozici.',
+    'warning_storage_full' => 'Pozor: sklad je plný. Balíček nelze aktivovat!',
+
+    // Buy messages
+    'buy_success' => 'Nákup dokončen. Předmět najdeš v inventáři.',
+    'buy_insufficient_dm' => 'Nedostatek Temné Hmoty.',
+    'label_dark_matter' => 'Temná Hmota',
+    'btn_buy' => 'Koupit',
+    'btn_confirm_buy' => 'Potvrdit nákup',
+    'price' => 'Cena',
+];
