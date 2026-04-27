140b6760ac21054998f2c92ff7490aea0d1a5140
piciolo <piciolo2@gmail.com>
2026-04-28 00:12:23 +0200

    feat(i18n): add SK (Slovak) Auctioneer + Shop translations from official OGame
    Source: official OGame s274-it.ogame.gameforge.com (UI=sk) - Dražiteľ + Obchod

---
+++ resources/lang/sk/t_shop_items.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+    // Categories
+    'category_special_offers' => 'Špeciálne ponuky',
+    'category_class_selection' => 'Voľba kategórie',
+    'category_construction' => 'Konštrukcia',
+    'category_resources' => 'Zdroje',
+    'category_booster30' => 'Posilnenie (30 dní)',
+    'category_booster90' => 'Posilnenie (90 dní)',
+    'category_profile' => 'Profil',
+    'category_items' => 'Všetko',
+
+    // Tabs
+    'tab_shop' => 'Obchod',
+    'tab_inventory' => 'Inventár',
+    'page_title_shop' => 'Obchod',
+    'page_title_inventory' => 'Inventár',
+
+    // Buttons
+    'btn_get_more_resources' => 'Získaj viacej surovín',
+    'btn_purchase_dark_matter' => 'Nákup Temnej hmoty',
+    'btn_activate' => 'Aktivovať',
+    'btn_buy_and_activate' => 'Kúpiť a aktivovať',
+    'btn_buy_at_cost' => 'Kúpiť za',
+
+    // Tiers
+    'tier_bronze' => 'Bronz',
+    'tier_silver' => 'Striebro',
+    'tier_gold' => 'Zlato',
+    'tier_platinum' => 'Platina',
+
+    // Labels
+    'label_duration' => 'Trvanie',
+    'label_inventory' => 'V inventári',
+    'label_inventory_short' => 'Inventár',
+    'label_time_left' => 'Zostávajúci čas',
+    'label_phase_in_use' => 'Aktuálna fáza: V použití',
+    'btn_extend' => 'Predĺžiť',
+    'label_price' => 'Cena',
+    'overlay_title' => 'Aktivovateľné položky',
+    'active_boosts_title' => 'Aktívne posilnenia',
+    'tooltip_rules_title' => 'Pravidlá',
+    'shop_link' => 'Do Obchodu/Inventára',
+    'duration_instant' => 'okamžite',
+    'duration_week' => '1 týždeň',
+    'unit_week' => 'týždeň',
+    'unit_day' => 'dni',
+
+    // Item titles
+    'booster_kraken_title' => 'Kraken',
+    'booster_newtron_title' => 'Newtron',
+    'booster_detroid_title' => 'Detroid',
+    'amplifier_metal_title' => 'Posilnenie kovu',
+    'amplifier_crystal_title' => 'Posilnenie kryštálu',
+    'amplifier_deuterium_title' => 'Posilnenie deutéria',
+    'amplifier_energy_title' => 'Posilnenie energie',
+    'resources_lot_title' => 'Balíček surovín',
+    'planet_fields_title' => 'Polia planéty',
+    'moon_fields_title' => 'Polia mesiaca',
+    'expedition_slot_title' => 'Sloty expedície',
+    'fleet_slot_title' => 'Sloty flotily',
+
+    // Item descriptions
+    'booster_kraken_desc' => 'Skracuje (:reduction) čas výstavby budov, ktoré sa aktuálne stavajú na planéte.',
+    'booster_newtron_desc' => 'Skracuje o :reduction čas výskumu aktívnych výskumov.',
+    'booster_detroid_desc' => 'Skracuje (:reduction) čas vykonávania úloh Lodenice, ktoré aktuálne prebiehajú na planéte.',
+    'amplifier_metal_desc' => '+:percent% viac ťažby Bane na kov na jednej planéte',
+    'amplifier_crystal_desc' => '+:percent% viac produkcie Bane na kryštál na jednej planéte',
+    'amplifier_deuterium_desc' => '+:percent% viac ťažby Syntetizéra deutéria na jednej planéte',
+    'amplifier_energy_desc' => '+:percent% viac produkcie energie na planéte',
+    'resources_lot_desc' => 'Príjmi pevný balíček surovín: :metal kovu, :crystal kryštálu, :deuterium deutéria.',
+    'planet_fields_desc' => 'Trvalo zväčší veľkosť planéty o :fields polí.',
+    'moon_fields_desc' => 'Trvalo zväčší veľkosť mesiaca o :fields polí.',
+    'expedition_slot_desc' => 'Zvýši počet súčasných expedícií o :slots.',
+    'fleet_slot_desc' => 'Zvýši počet súčasných flotíl o :slots.',
+
+    // Activation messages
+    'activate_success' => 'Položka aktivovaná.',
+    'activate_not_found' => 'Položka nie je dostupná v inventári.',
+    'activate_not_supported' => 'Aktivácia ešte nie je dostupná pre túto položku.',
+    'activate_no_target' => 'Žiadny platný cieľ pre aktiváciu (prázdny rad alebo planéta nedostupná).',
+
+    // Empty states
+    'inventory_empty' => 'Tvoj inventár je prázdny.',
+    'shop_coming_soon' => 'Obchod s Temnou hmotou bude čoskoro dostupný.',
+    'warning_storage_full' => 'Pozor: sklad je plný. Balíček nie je možné aktivovať!',
+
+    // Buy messages
+    'buy_success' => 'Nákup dokončený. Položku nájdeš v inventári.',
+    'buy_insufficient_dm' => 'Nedostatok Temnej hmoty.',
+    'label_dark_matter' => 'Temná hmota',
+    'btn_buy' => 'Kúpiť',
+    'btn_confirm_buy' => 'Potvrdiť nákup',
+    'price' => 'Cena',
+];
